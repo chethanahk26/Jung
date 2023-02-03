4cc0bd02f3727697fa97fa562cebf05e2761441b
brundamt12 <brundamt@gmail.com>
2023-02-03 20:53:29 +0530

    yes

---
+++ Demo12.java
@@ -0,0 +1,17 @@
+class Demo13
+{
+static void sector()
+{
+int r = 10;
+final double tita = 30.5;
+double sector = 0.5*r*r*tita;
+System.out.println("Area of sector is" + area);
+return;
+}
+public static void main(String[] args)
+{
+System.out.println("------main starts------");
+sector();
+System.out.println("------main ends------");
+}
+}
