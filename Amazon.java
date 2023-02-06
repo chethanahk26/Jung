555e4874462daad838a192ff68b82762fc85a363
brundatm12 <brundamt@gmail.com>
2023-02-06 20:54:12 +0530

    hello

---
+++ Amazon.java
@@ -0,0 +1,33 @@
+class Amazon1
+{
+static void signup()
+{
+System.out.println("signing up");
+}
+static void login()
+{
+System.out.println("logging in");
+}
+public static void main(Strind[] args)
+{
+System.out.println("------welcome to amazon------");
+signup();
+login();
+home page();
+order();
+logout();
+}
+static void home page()
+{
+System.out.println("home page");
+}
+static void order()
+{
+System.out.println("order");
+}
+static void logout()
+{
+System.out.println("logout");
+}
+}
+
