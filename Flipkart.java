e51056d7a0793f9f07e10096582e760bd6d7735a
brundatm12 <brundamt@gmail.com>
2023-02-06 20:41:24 +0530

    hi

---
+++ Flipkart.java
@@ -0,0 +1,32 @@
+class Flipkart1
+{
+static void Signup()
+{
+System.out.println("Signing up");
+}
+static void homepage()
+{
+System.out.println("homepage");
+}
+public static void main(String[] args)
+{
+System.out.println("------welcome to Amazon------");
+Signup();
+login();
+homepage();
+order();
+logout();
+}
+static void login()
+{
+System.out.println("logging in");
+}
+static void order()
+{
+System.out.println("order");
+}
+static void logout()
+{
+System.out.println("logout");
+}
+}
